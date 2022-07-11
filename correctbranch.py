a51b0df6b2d83b2f4a050f18e84258927f9642cb
KKRISHNA SAXENA <saxena.kkrishna@gmail.com>
2022-07-11 23:28:19 +0530

    Add files via upload

---
+++ correctbranch.py
@@ -0,0 +1,67 @@
+from models import *
+
+
+def CorrectedBranch(enteredBranch):
+    enteredBranch = enteredBranch.lower()
+    enteredBranch = str(enteredBranch)
+    print("current EnteredBranch is " + enteredBranch)
+
+    if(enteredBranch in [x.lower() for x in aeroWords]):
+        enteredBranch = "Aeronautical"
+
+    elif(enteredBranch in [x.lower() for x in autoWords]):
+        enteredBranch = "Automobile"
+
+    elif(enteredBranch in [x.lower() for x in biomedWords]):
+        enteredBranch = "Biomedical"
+
+    elif(enteredBranch in [x.lower() for x in biotechWords]):
+        enteredBranch = "Biotechnology"
+
+    elif(enteredBranch in [x.lower() for x in chemWords]):
+        enteredBranch = "Chemical"
+
+    elif(enteredBranch in [x.lower() for x in civilWords]):
+        enteredBranch = "Civil"
+
+    elif(enteredBranch in [x.lower() for x in cceWords]):
+        enteredBranch = "CCE"
+
+    elif(enteredBranch in [x.lower() for x in cseWords]):
+        enteredBranch = "CSE"
+
+    elif(enteredBranch in [x.lower() for x in dseWords]):
+        enteredBranch = "DSE"
+
+    elif(enteredBranch in [x.lower() for x in eeWords]):
+        enteredBranch = "EEE"
+
+    elif(enteredBranch in [x.lower() for x in eceWords]):
+        enteredBranch = "ECE"
+
+    elif(enteredBranch in [x.lower() for x in eieWords]):
+        enteredBranch = "EIE"
+
+    elif(enteredBranch in [x.lower() for x in ipWords]):
+        enteredBranch = "Industrial Production"
+
+    elif(enteredBranch in [x.lower() for x in itWords]):
+        enteredBranch = "IT"
+
+    elif(enteredBranch in [x.lower() for x in mechWords]):
+        enteredBranch = "Mechanical"
+
+    elif(enteredBranch in [x.lower() for x in mechxWords]):
+        enteredBranch = "Mechatronics"
+
+    elif(enteredBranch in [x.lower() for x in mediaWords]):
+        enteredBranch = "Media Technology"
+
+    elif(enteredBranch in [x.lower() for x in aiWords]):
+        enteredBranch = "AI"
+
+    elif(enteredBranch in [x.lower() for x in cpsWords]):
+        enteredBranch = "Cyber Physical Systems"
+
+    print("The corrected is " + enteredBranch)
+    return enteredBranch
---
+++ models.py
@@ -0,0 +1,37 @@
+aeroWords = ["Aero", "Aeronautical", "Aerospace", "Aeronautical Engineering"]
+
+autoWords = ["Automobile", "AutoM", "Automobile Engineering"]
+
+biomedWords = ["biomed", "biomedical","biomedical engineering", "BioM", "Bio Medical Engineering"]
+
+biotechWords = ["biotech", "BioT", "Bio Technology", "biotechnology"]
+
+chemWords = ["Chem", "Chemical", "Chemistry", "Chemical Engineering"]
+
+civilWords = ["civil", "Civil Engineering"]
+
+cceWords = ["cce", "Communication and Computer Engineering", "cc"]
+
+cseWords = ["CS", "CSE", "Computer Science","Computer Sci", "Computer Science and Engineering"]
+
+dseWords = ["DS", "DSE", "Data Science Engineering", "Data Science"]
+
+eeWords = ["Electrical", "ee", "eee", "Electrical and Electronics Engineering"]
+
+eceWords = ["ece", "ec" "Electronics and Communication Engineering", "Electronics"]
+
+eieWords = ["eie", "instrumentation", "EandI", "E&I","Electronics and Instrumentation Engineering"]
+
+ipWords = ["ip", "industrial", "production","industrial production", "Industrial and Production Engineering"]
+
+itWords = ["IT", "Information Technology", "InfoTech"]
+
+mechWords = ["mech", "mechanical", "MechE", "Mechanical Engineering", "ME"]
+
+mechxWords = ["mechatronics", "MechX", "MechT", "Mechatronics Engineering"]
+
+mediaWords = ["MediaTech", "Media Tech", "Media Technology", "Media"]
+
+aiWords = ["AI", "CSAI", "Articial Intelligence"]
+
+cpsWords = ["Cyber", "Physical", "Cyber System", "Cyber Physical System","Cyber Physical Systems","CPS"]
